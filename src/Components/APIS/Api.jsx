cb2d34085a9d83fdacfde8e9862395d3b3820e17
siddique 313 <siddiqueali531@gmail.com>
2025-02-07 16:53:29 +0000

    all done

--- src/Components/APIS/Api.jsx
+++ src/Components/APIS/Api.jsx
@@ -3,21 +3,20 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
 });
-export const fetchPost = async () => {
-  // try {
-  const res = await api.get("/posts");
-
-  return res?.status === 200 ? res?.data : [];
-  // } catch (error) {
-  // console.log(error);
-  // }
+export const fetchPost = async (pageNumber) => {
+  try {
+    const res = await api.get(`posts?_start=${pageNumber}&_limit=3`);
+    return res?.status === 200 ? res?.data : [];
+  } catch (error) {
+    console.log(error);
+  }
 };
 export const FetchIndvPost = async (id) => {
-  // try {
-  const res = await api.get(`/posts/${id}`);
-  res.status === 200 ? res.data : [];
-  return res?.data;
-  // } catch (error) {
-  // console.log(error);
-  // }
+  try {
+    const res = await api.get(`/posts/${id}`);
+    res.status === 200 ? res.data : [];
+    return res?.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
--- src/Components/pages/FetchRQ.jsx
+++ src/Components/pages/FetchRQ.jsx
@@ -1,11 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { fetchPost } from "../APIS/Api";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function FetchRQ() {
+  const [pageNumber, setPageNumber] = useState(1);
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPost,
+    queryKey: ["posts", pageNumber],
+    queryFn: () => fetchPost(pageNumber),
+    placeholderData: keepPreviousData,
   });
 
   console.log({ error });
@@ -20,23 +23,45 @@ function FetchRQ() {
     );
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 text-black">
-      {data?.map((curElem, index) => {
-        const { body, title, id } = curElem;
-        return (
-          <div
-            key={index}
-            className="bg-white p-6 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300"
-          >
-            <NavLink to={`/rq/${id}`}>
-              <p className="text-4xl text-center font-mono"> ID No : {id}</p>
-              <h1 className="text-2xl font-semibold mb-4">{title}</h1>
-              <p className="text-gray-700 text-sm">{body}</p>
-            </NavLink>
-          </div>
-        );
-      })}
-    </div>
+    <section>
+      <ol className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 text-black list-inside list-decimal">
+        {data?.map((curElem, index) => {
+          const { body, title, id } = curElem;
+          return (
+            <li
+              key={index}
+              className="  h-72 bg-gray-800 text-white rounded-lg shadow-lg p-6 space-y-4 border-l-2 border-white  flex flex-col justify-around gap-7 items-center"
+            >
+              <NavLink to={`/rq/${id}`}>
+                <p className="text-4xl text-center font-mono"> ID No : {id}</p>
+                <h1 className="text-2xl font-semibold mb-4">{title}</h1>
+                <p className=" text-sm">{body}</p>
+              </NavLink>
+            </li>
+          );
+        })}
+      </ol>
+      <div className="w-full flex gap-4 items-center">
+        <button
+          className={`px-10 py-3 bg-violet-700 rounded-2xl ${
+            pageNumber <= 1 ? "bg-lime-500" : false
+          }`}
+          disabled={pageNumber <= 1 ? true : false}
+          onClick={() => setPageNumber((pre) => pre - 3)}
+        >
+          Prev
+        </button>
+        <h1>{(pageNumber / 3 + 1).toFixed(0)}</h1>
+        <button
+          className={`px-10 py-3 bg-violet-700 rounded-2xl  ${
+            pageNumber <= 1 ? "bg-lime-500" : "bg-orange-700"
+          }`}
+          onClick={() => setPageNumber((pre) => pre + 3)}
+        >
+          Next
+        </button>
+      </div>
+    </section>
   );
 }
 
